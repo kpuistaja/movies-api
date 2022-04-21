5a82dc5c95350b8d71af6c0af4cc1eee83c3f38f
kpuistaja <Kadi.Puistaja@tptlive.ee>
2022-04-21 13:04:58 +0300

    Delete a movie

--- index.js
+++ index.js
@@ -47,6 +47,16 @@ app.post('/movies', (req, res) => {
         .send(movie)
 })
 
+app.delete('/movies/:id', (req, res) => {
+    if (typeof movies[req.params.id - 1] === 'undefined') {
+        return res.status(404).send({error: "Movie not found"})
+    }
+
+    movies.splice(req.params.id - 1, 1)
+
+    res.status(204).send({error: "No content"})
+})
+
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(port, () => { 
